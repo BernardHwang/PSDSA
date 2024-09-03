8d5315b39415ea9663f860a1d5de47dfb3341981
BernardHwang <147620186+BernardHwang@users.noreply.github.com>
2024-09-03 13:37:12 +0800

    Update SortingAlgorithms.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -60,7 +60,7 @@ public class SortingAlgorithms {
         }
     }
 
-    public static void selectionSortWord(List<String> list) {
+    public static void selectionSortWords(List<String> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < list.size(); j++) {
