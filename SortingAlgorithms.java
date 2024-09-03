c86c551f955408be84481e5dac933235c0d1f880
BernardHwang <147620186+BernardHwang@users.noreply.github.com>
2024-09-03 12:36:55 +0800

    Update SortingAlgorithms.java

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -43,7 +43,7 @@ public class SortingAlgorithms {
     }
 
     // Optimized for ArrayList
-    public static void selectionSortNumber(ArrayList<Integer> list) {
+    public static void selectionSortNumber(List<Integer> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < list.size(); j++) {
@@ -53,14 +53,14 @@ public class SortingAlgorithms {
             }
             // Swap the elements
             if (minIndex != i) {
-                T temp = list.get(minIndex);
+                int temp = list.get(minIndex);
                 list.set(minIndex, list.get(i));
                 list.set(i, temp);
             }
         }
     }
 
-    public static void selectionSortWord(ArrayList<String> list) {
+    public static void selectionSortWord(List<String> list) {
         for (int i = 0; i < list.size() - 1; i++) {
             int minIndex = i;
             for (int j = i + 1; j < list.size(); j++) {
@@ -70,7 +70,7 @@ public class SortingAlgorithms {
             }
             // Swap the elements
             if (minIndex != i) {
-                T temp = list.get(minIndex);
+                String temp = list.get(minIndex);
                 list.set(minIndex, list.get(i));
                 list.set(i, temp);
             }
@@ -78,7 +78,7 @@ public class SortingAlgorithms {
     }
 
     // Optimized for LinkedList
-    public static Node selectionSort(Node head) {
+    public static Node selectionSortNode(Node head) {
         Node temp = head; 
   
         // Traverse the List 
