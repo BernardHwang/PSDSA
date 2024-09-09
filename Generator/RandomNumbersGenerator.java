a94fe9ec1a67a90e4e06801be26e59a18f6d6546
kok847 <148704845+kok847@users.noreply.github.com>
2024-09-09 20:41:47 +0800

    Update RandomNumbersGenerator.java

--- Generator/RandomNumbersGenerator.java
+++ Generator/RandomNumbersGenerator.java
@@ -8,12 +8,12 @@ public class RandomNumbersGenerator {
     public static void main(String[] args) throws IOException {
         Random random = new Random();
         ArrayList<Integer> numList = new ArrayList<>(10);
-        int numbers = 10000000;
+        int numbers = 20000000;
         
         // Use try-with-resources to ensure FileWriter is closed
         try (FileWriter writer = new FileWriter("random_numbers.txt")) {
             for (int i = 0; i < numbers; i++) {
-                int numDigits = random.nextInt(7) + 1;
+                int numDigits = random.nextInt(8) + 1;
                 int min = (int) Math.pow(10, numDigits - 1);
                 int max = (int) Math.pow(10, numDigits) - 1;
                 int number = random.nextInt((max - min) + 1) + min;
